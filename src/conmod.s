d1fb027a1bedde12ac50301ace4677fc67c106c7
kg68k <78926718+kg68k@users.noreply.github.com>
2025-04-23 23:29:09 +0900

    chore: bump version to 1.0.0

--- src/conmod.s
+++ src/conmod.s
@@ -17,7 +17,7 @@
 ;You should have received a copy of the GNU General Public License
 ;along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-VERSION_STRING: .reg '1.0.0-beta.1'
+VERSION_STRING: .reg '1.0.0'
 
 
 .include macro.mac
